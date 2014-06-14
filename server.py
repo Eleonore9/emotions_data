a1b6151e14b4137b35eae1acfb7e4d6683a63732
Eleonore9 <eleonore.mayola@ymail.com>
2014-06-14 17:45:09 +0200

    commented out the stuffs for the ajax call I'm not using

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 import os, sys
 from flask import Flask, render_template, redirect, request, session 
 import model
-import requests
+#import requests
 import json
 
 app= Flask(__name__)
@@ -28,11 +28,11 @@ def emotions():
     #vids = model.Element.get_all_videos("video")
     return render_template('emotions.html', elements=elements)
 
-@app.route('/emotions/music')
-def get_music():
-    url = "http://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key=de7fb3597dd8746a57b068228cb9f986&format=json"
-    r = requests.get(url)
-    return json.dumps(r.json())
+#@app.route('/emotions/music')
+#def get_music():
+    #url = "http://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key=de7fb3597dd8746a57b068228cb9f986&format=json"
+    #r = requests.get(url)
+    #return json.dumps(r.json())
 
 app.secret_key = 'this_a_non_secret_key'
 
