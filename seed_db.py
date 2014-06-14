cd05abde26971668b248350bad508bd3a9e60a96
Eleonore9 <eleonore.mayola@ymail.com>
2014-06-14 09:03:34 +0200

    get a type error when adding a new ellement object to the element database

--- seed_db.py
+++ seed_db.py
@@ -3,11 +3,10 @@ import model
 #user1 = model.User.new("eleonore.mayola@ymail.com", "password", "Eleonore")
 
 element2 = model.Element.new(
-                    None,
                     "ladybird on flower", 
                     "http://media-cache-ec0.pinimg.com/736x/4f/87/7d/4f877d0fa46f94b1b260b0ae8c4924f8.jpg",
                     "image",
                     "sad",
-                    "",
+                    "cloudy",
                     1
                     )
