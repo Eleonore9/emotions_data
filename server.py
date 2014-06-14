b1a5d37d7bb8c85a47df464fbd2e37f162562c73
Eleonore9 <eleonore.mayola@ymail.com>
2014-06-14 10:04:35 +0200

    added templates and bootstrap css

--- server.py
+++ server.py
@@ -3,22 +3,6 @@
 import os, sys
 from flask import Flask, render_template, redirect, request, session 
 import model
-#import sqlite3
-#from flask import g
-
-#DATABASE = 'emotion_data.db.db'
-
-#def get_db():
-#    db = getattr(g, '_database', None)
-#    if db is None:
-#        db = g._database = sqlite3.connect(DATABASE)
-#        return db
-
-#@app.teardown_appcontext
-#    def close_connection(exception):
-#        db = getattr(g, '_database', None)
-#        if db is not None:
-#            db.close()
 
 app= Flask(__name__)
 
@@ -28,8 +12,9 @@ def teardown_request(exc):
 
 @app.route('/')
 def index():
-    user = model.User.get(1)
-    return 'Hello %s!' %user.username
+    #user = model.User.get(1)
+    #return 'Hello %s!' %user.username
+    return render_template('index.html')
 
 
 app.secret_key = 'this_a_non_secret_key'
