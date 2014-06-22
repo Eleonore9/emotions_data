c9d59cedce2271adc237e19dc7668a97c19a0b76
Eleonore9 <eleonore.mayola@ymail.com>
2014-06-23 01:17:22 +0200

    trying to implement masonry but it prevents jquery to work now

--- static/js/emotions.js
+++ static/js/emotions.js
@@ -1,23 +1,25 @@
-function GetTracks() {
-  $.getJSON('/emotions/music', function(data){
-    console.log(data); window.data = data;
-    $.each(data.toptags.tag, function(index, element) {
-      if(element.name == 'rock'){
-	console.log(element.url);
-        $('#music').html('<p>' + element.url + '</p>');
-        $('#music').html('<embed height="50" width="100" src="http://' + element.url + '">');
+//function GetTracks() {
+  //$.getJSON('/emotions/music', function(data){
+    //console.log(data); window.data = data;
+    //$.each(data.toptags.tag, function(index, element) {
+      //if(element.name == 'rock'){
+	//console.log(element.url);
+        //$('#music').html('<p>' + element.url + '</p>');
+        //$('#music').html('<embed height="50" width="100" src="http://' + element.url + '">');
 
-      }
-    });
-  });
-};
+      //}
+    //});
+  //});
+//};
+
+var container = document.querySelector('#container');
+var msnry = new Masonry( container, {
+    // options
+    columnWidth: 200,
+    itemSelector: '.item'
+});
 
 $(document).ready(function() {
-  //$('#music .btn').click(function(e) {
-//console.log("click", e);
-//e.preventDefault();
-   //GetTracks();
-  //});
   $('.jumbotron .btn').click(function(){
     $('.jumbotron').append('<p> Are you ready?</p>');
     $('.jumbotron').append('<p><a class="btn btn-lg btn-success" href="/emotions" role="button">Yes</a></p>')
@@ -27,6 +29,3 @@ $(document).ready(function() {
     });
   });
 })
-
-//console.log('load emotions');
-
