d3deffc17733f9529b7695b98b79822fd2ac6954
Eleonore9 <eleonore.mayola@ymail.com>
2014-09-01 18:36:43 +0200

    it works when searching by css class but then I cant access all the pins without being logged in

--- scrape_bs.py
+++ scrape_bs.py
@@ -2,6 +2,7 @@
 
 from bs4 import BeautifulSoup 
 import requests
+import sys
 
 DEBUG = True
 
@@ -11,8 +12,12 @@ page = requests.get('http://www.pinterest.com/eleonore9/photography/')
 if page.status_code == 200:
     if DEBUG:
         print 2
-    #print page.text[:100]
     soup = BeautifulSoup(page.text)
-    links = soup.a
-    if DEBUG:
-        print links
+    #for item in soup.find_all('a', attrs={"class": u"pinImageWrapper"}):
+    sys.stdout = open('print.html', 'w')
+    items = soup.find_all("a", class_="pinImageWrapper")
+    print '* ', len(items)
+    for item in items:
+        if DEBUG:
+            #sys.stdout = open('print.html', 'w')
+            print item 
