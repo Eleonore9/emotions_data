800792680efd978f40592a869fbbd775224dc68c
Eleonore9 <eleonore.mayola@ymail.com>
2014-09-01 16:22:46 +0200

    still working on the bs scraping script to select the right links from the page

--- scrape_bs.py
+++ scrape_bs.py
@@ -1,12 +1,18 @@
-#Try to scrape using beautiful soup
+#Try to scrape using beautiful soup library
 
 from bs4 import BeautifulSoup 
 import requests
 
-print 1
+DEBUG = True
+
+if DEBUG:
+    print 1
 page = requests.get('http://www.pinterest.com/eleonore9/photography/')
 if page.status_code == 200:
-    print 2
+    if DEBUG:
+        print 2
     #print page.text[:100]
     soup = BeautifulSoup(page.text)
-    print type(soup.a)
+    links = soup.a
+    if DEBUG:
+        print links
