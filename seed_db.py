e07befc7cf74640d8e85ac29849fc1566f213859
Eleonore9 <eleonore.mayola@ymail.com>
2015-05-13 00:21:37 +0100

    cleaning up, getting project running again

--- seed_db.py
+++ seed_db.py
@@ -1,12 +1,12 @@
 import model
 
-#user1 = model.User.new("eleonore.mayola@ymail.com", "password", "Eleonore")
+user1 = model.User.new("eleonore.mayola@ymail.com", "password", "Eleonore")
 
-e5 = model.Element.new(
-                    "autumn in NYc", 
-                    "https://www.dropbox.com/s/s8nxzvjq80m8dyn/AutumnInNewYork%20%28nancy%20perlman%27s%20conflicted%20copy%202014-06-14%29.mp4",
-                    "video",
-                    "sad",
-                    "cloudy",
-                    1
-                    )
+# e5 = model.Element.new(
+#                     "autumn in NYc", 
+#                     "https://www.dropbox.com/s/s8nxzvjq80m8dyn/AutumnInNewYork%20%28nancy%20perlman%27s%20conflicted%20copy%202014-06-14%29.mp4",
+#                     "video",
+#                     "sad",
+#                     "cloudy",
+#                     1
+#                     )
--- server.py
+++ server.py
@@ -23,8 +23,8 @@ def index():
 
 @app.route('/emotions')
 def emotions():
-    #elements = model.Element.get_all()
-    elements = model.Element.get_all_emotion("sad")
+    elements = model.Element.get_all()
+    #elements = model.Element.get_all_emotion("sad")
     return render_template('emotions.html', elements=elements)
 
 #@app.route('/emotions/music')
